dc9e7b2a6b0998c0f08b63869f7efca43a663c69
JesterXL <jessew@webappsolution.com>
2013-04-12 15:47:22 -0400

    test for logger 2

---
+++ test/FlowMVC/logger/TestLogger2.js
@@ -0,0 +1,16 @@
+describe("FlowMVC.logger.Logger2", function()
+{
+    beforeEach(function()
+    {
+	});
+
+    afterEach(function()
+    {
+    });
+
+    it("should instantiate", function()
+    {
+    	var instance = Ext.create("FlowMVC.logger.Logger2");
+    	expect(instance).not.toBeNull();
+    });
+});
