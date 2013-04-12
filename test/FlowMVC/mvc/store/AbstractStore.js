0faa52660d239478e9abd1a09bee3d93059dbb24
brianmriley <brianr@webappsolution.com>
2013-04-12 09:49:41 -0400

    * fixed copyright in all flow test cases. working on event testing for abstract store.

--- test/FlowMVC/mvc/store/AbstractStore.js
+++ test/FlowMVC/mvc/store/AbstractStore.js
@@ -1,18 +1,18 @@
 /*
  Copyright (c) 2013 [Web App Solution, Inc.](mailto:admin@webappsolution.com)
 
- CafeTownsend Sencha Touch DeftJS PoC is free software: you can redistribute it and/or modify
+ FlowMVC is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- CafeTownsend Sencha Touch DeftJS PoC is distributed in the hope that it will be useful,
+ FlowMVC is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with CafeTownsend Sencha Touch DeftJS PoC.  If not, see <http://www.gnu.org/licenses/>.
+ along with FlowMVC.  If not, see <http://www.gnu.org/licenses/>.
  */
 describe("FlowMVC.mvc.store.AbstractStore", function() {
 
@@ -132,6 +132,21 @@ describe("FlowMVC.mvc.store.AbstractStore", function() {
             expect(store.fireEvent).toHaveBeenCalledWith("selectedRecordChange", store, mockModel);
             expect(store.fireEvent).toHaveBeenCalledWith("selectedRecordChange", store, store.getSelectedRecord());
         });
+
+	    it("should have raised an event for a selected record and handled by a listener function of another object", function() {
+
+		    var listener = {
+			    onSelectedRecordChange: function(store, record) {
+				    console.error("onSelectedRecordChange");
+			    }
+		    };
+
+		    store.addListener("selectedRecordChange", listener.onSelectedRecordChange)
+		    spyOn(listener, "onSelectedRecordChange").andCallThrough();
+		    store.setSelectedRecord(mockModel);
+		    expect(listener.onSelectedRecordChange).toHaveBeenCalled();
+//		    expect(listener.onSelectedRecordChange).toHaveBeenCalledWith(store, mockModel);
+	    });
 
     });
 
